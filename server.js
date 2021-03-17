7b924078475d09aaccc12f3572aa0a7a9e39813e
lauticouget <lauticouget@gmail.com>
2021-03-17 16:11:47 -0300

    master | cors config

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // require('rootpath')();
 const express = require('express');
 const app = express();
-// const cors = require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const authMiddleware = require('./middlewares/authMiddleware');
 const errorHandler = require('./helpers/errorHandler');
@@ -9,7 +9,7 @@ const errorHandler = require('./helpers/errorHandler');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.use(jwt());
+app.use(cors());
 app.use('/api', authMiddleware(), [
     require('./users/userController'),
     require('./todos/todosController')
